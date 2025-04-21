3b6a0939f4e3877bafb2bb207fb29847164aebd4
Alondra Gamez <agamez@appdev.yearup.org>
2025-04-21 13:02:05 -0500

    Completed Exercise 1 - Looping Exercises

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        //do/while loop
+        int i = 5;
+        do {
+            System.out.println("I love Java");
+            i--;
+        }
+        while (i > 0);
+    }
+}
---
+++ LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        //while loop
+        int i = 5;
+        while (i > 0) {
+            System.out.println("I love Java");
+
+            i--;
+        }
+    }
+}
